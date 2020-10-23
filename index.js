beee7e1a01369d0c762ee68a114cb67a3673bcb5
Martin Achtermann <mdacht777@gmail.com>
2020-10-23 22:11:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function countDown(x){
+let countup = x;
+while (countup >= 0) {
+  console.log(countup--);
+}
+}
+
+function writeCards(x,y){
+const out = [];
+  for (let i = 0; i < x.length; i++) {
+    out.push(`Thank you, ${x[i]}, for the wonderful ${y} gift!`);
+  }
+ 
+  return out;
+
+}
